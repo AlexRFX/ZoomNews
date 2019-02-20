90f261a7b756a861542cf72a712820ccd588e366
AlexRFX <36972541+AlexRFX@users.noreply.github.com>
2019-02-20 12:43:43 -0300

    Inicio do projeto.

---
+++ footer.php
@@ -0,0 +1,20 @@
+<head>
+    <style>
+        /* Set black background color, white text and some padding */
+        footer {
+            background-color: #000000;
+            color: white;
+            padding: 15px;
+        }
+    </style>
+</head>
+
+<footer class="container-fluid text-center">
+    <div class="row">
+        <div class="col-md-12">
+            <h3>Repository:</h3>
+            <p><a href="https://github.com/AlexRFX"><img border="0" alt="Github's Repository" src="imgs/github.png" width="100" height="100"></p>
+        </div>
+    </div>
+</footer>
+
---
+++ navbar.php
@@ -0,0 +1,36 @@
+<head>
+    <style>
+        /* Set black background color, white text and some padding */
+        .col-md-12 {
+            background-color: #000000;
+            color: white;
+            padding: 15px;
+        }
+    </style>
+</head>
+
+<div class="row">
+            <div class="col-md-12">
+                <img src="imgs/logo.gif" alt="Logo" style="width:20%">
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Quem somos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Contato</a>
+                    </li>
+                    <li class="nav-item dropdown ml-md-auto">
+                        <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Dropdown link</a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
+                            <div class="dropdown-divider">
+                            </div> <a class="dropdown-item" href="#">Separated link</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
